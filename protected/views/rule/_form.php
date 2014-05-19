d9f51a48220ea794795d9030c5ae39b7838a92f2
manu <manu@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-05-19 13:48:43 +0000

    mentis 0000495: Rules and Forms drop down list showing multiple versions

--- protected/views/rule/_form.php
+++ protected/views/rule/_form.php
@@ -25,8 +25,37 @@
 		<?php echo $form->error($model,'title'); ?>
 	</div>
 	<div class="row">
+
 		<?php echo $form->labelEx($model,'text'); ?>
-		<?php echo $form->textArea($model,'text',array('rows'=>6, 'class'=>'span8')); ?>
+    <?php $this->widget('application.extensions.tinymce.ETinyMce',
+                        array('model'=>$model,
+                              'attribute'=>'text',
+                              'editorTemplate'=>'full',
+                              'htmlOptions'=>array('rows'=>6, 'cols'=>50, 'class'=>'tinymce'),
+                               'options' => array(
+//                                   'plugins' => 'jbimages,filemanager,save,media',
+                                   'mode' =>"textareas",
+    'theme_advanced_disable' => "image",
+                                   'mceRemoveControl'=>true,
+                                   //'cleanup' => false,
+                                    'language'=>"en",
+                                    'theme_advanced_buttons1'=>"save,filemanager,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+                                    'theme_advanced_buttons2'=> "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,jbimages,insertfile,media,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+                                    'theme_advanced_buttons3'=>"tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+                                    'theme_advanced_buttons4'=>"insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+                                    'theme_advanced_toolbar_location' => 'top',
+                                    'theme_advanced_toolbar_align' => 'left',
+//                                    'theme_advanced_statusbar_location' => 'bottom',
+                                    'theme_advanced_resizing' => true,
+                                    'relative_urls' => false,
+                                   'media_strict' => true,
+                                    'document_base_url' => Yii::app()->getBaseUrl(true),
+                                    'theme_advanced_font_sizes' => "10=10pt,11=11pt,12=12pt,13=13pt,14=14pt,15=15pt,16=16pt,17=17pt,18=18pt,19=19pt,20=20pt",
+//                                   'extended_valid_elements'=> "iframe[src|class|width|height|name|align]",
+                                   'extended_valid_elements' => "iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder]",
+                                   ),
+                            )); 
+?>
 		<?php echo $form->error($model,'text'); ?>
 	</div>
 
@@ -37,3 +66,6 @@
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
+<style>
+  #Rule_text_switch{display:none;}
+  </style>
