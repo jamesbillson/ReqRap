d9545695b3483ad2f5b76bfe1effe297fec16d02
manu <manu@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-06-18 11:31:23 +0000

    Alert system - Partial working code

---
+++ data/alert_schema.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 18, 2014 at 11:25 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rqq`
+--
+CREATE DATABASE IF NOT EXISTS `rqq` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `rqq`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `scope` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `exclude` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `condition` text COLLATE utf8_unicode_ci NOT NULL,
+  `show_once` tinyint(1) NOT NULL,
+  `type` tinyint(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `scope`, `exclude`, `condition`, `show_once`, `type`) VALUES
+(1, 'Please complete your profile', '*/*', '', '', 0, 0),
+(2, 'new message goes in here', 'usecase/view', '', '', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meta`
+--
+
+CREATE TABLE IF NOT EXISTS `user_meta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `has_viewed` tinyint(1) NOT NULL,
+  `has_acknowledged` tinyint(1) NOT NULL,
+  `alert_messages_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `alert_messages_id` (`alert_messages_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `user_meta`
+--
+
+INSERT INTO `user_meta` (`id`, `user_id`, `has_viewed`, `has_acknowledged`, `alert_messages_id`) VALUES
+(1, 115, 0, 0, 1),
+(2, 115, 0, 0, 2);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_meta`
+--
+ALTER TABLE `user_meta`
+  ADD CONSTRAINT `user_meta_ibfk_1` FOREIGN KEY (`alert_messages_id`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user_meta_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
