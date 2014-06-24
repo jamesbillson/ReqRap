b55c9f9d4fcc718da7167f7bec33733ce8aaafd2
bfconsult <bfconsult@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-06-24 03:50:10 +0000

    remove debug echo

---
+++ protected/views/usecase/packchange.php
@@ -0,0 +1,56 @@
+ <?php echo $this->renderPartial('/project/head',array('tab'=>'usecases')); 
+
+$permission=(Yii::App()->session['permission']==1)?true : false; 
+$packages = Package::model()->getPackages($model->id);
+$model_package=Package::model()->findbyPK(Version::model()->getVersion($model->package_id,5));   
+   
+?>
+<h2>Change Package </h2>
+<h3>UC-<?php echo str_pad($model_package['number'], 2, "0", STR_PAD_LEFT)
+        .str_pad($model->number, 3, "0", STR_PAD_LEFT).
+        '-'.$model->name; ?>
+</h3>
+<?php if($permission){ ?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'usecase-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	
+	<?php echo $form->errorSummary($model); ?>
+
+	<?php echo $form->hiddenField($model,'usecase_id',array('value'=>$model->usecase_id)); ?>
+
+    	<div class="row">
+         <?php echo $form->labelEx($model,'package_id'); ?>
+         <?php     
+               $select = array();
+               foreach($packages as $package)
+                      $select[$package['package_id']]=$package['name'];
+               echo $form->dropdownlist($model,'package_id',$select,array('prompt'=>'select')); ?> 
+               </div>          
+                        
+                        
+                        
+                    
+                      
+    <br />
+
+
+
+        
+        
+        
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
+<?php } ?> 
+
+
