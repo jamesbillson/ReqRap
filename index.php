af216d2442e5eca883e88e54e7d1e1b06a11bc0a
lannguyen <lannguyen@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-08-05 04:30:02 +0000

    revert index file

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-ini_set('memory_limit', '-1');
-ini_set('max_execution_time', 300);
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yiilite.php';
-$config = dirname(__FILE__).'/protected/config/main.php';
-
-
+$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
