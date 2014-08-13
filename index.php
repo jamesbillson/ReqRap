d184fecaaa93b0d68eb5e10f5cca17c92f198fc1
lannguyen <lannguyen@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-08-13 04:54:27 +0000

    remove worpdress

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
-ini_set('memory_limit', '-1');
-ini_set('max_execution_time', 300);
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-// wordpress config
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
 define('WP_USE_THEMES', true);
-$wp_did_header = true;
-require_once('wordpress/wp-load.php');
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yiilite.php';
-$config = dirname(__FILE__).'/protected/config/main.php';
-
-
-
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-require_once($yii);
-Yii::createWebApplication($config)->run();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
