9ead3aaf99b9c2c2a9d1bb54e78375183e10a967
bfconsult <bfconsult@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-12-04 02:34:51 +0000

    fix formatting as per #839

--- req/protected/views/flow/print.php
+++ req/protected/views/flow/print.php
@@ -2,30 +2,34 @@
 $flows = Flow::model()->getUCFlow($usecase); // get flows
 foreach($flows as $flow) { // LOOP THRU FLOWS
 $steps= Step::model()->getFlowSteps($flow['flow_id']); // get steps
-$name = ($flow['main']==1)?'<strong>Scenario Text</strong> ':'<strong>Alternate Course '.$flow['name'].
-        '</strong><br />Start after main step '.$flow['start'].
-        ' re-join at main step '.$flow['rejoin'];
+if($flow['main']==1){
+    $name = '<strong>Scenario Text</strong> ';
+} ELSE {
+    $name = '<strong>Alternate Course '.$flow['name'].
+            '</strong><br />Start after main step '.$flow['start'].
+            ' re-join at main step '.$flow['rejoin'];
+}
 ?>
          <tr> 
-             <td rowspan="<?php echo count($steps);?>">
+             
+             <td <?php if (count($steps)>1) echo 'rowspan="'.count($steps).'"'; ?>>
                <?php echo $name;?>
              </td>    
                     
                         
                
          <?php   
-         
+         $counter=0;
          foreach($steps as $step) { 
-                 
-         if($step['number']!=1) echo '<tr>';
+                 $counter++;
+         if($counter>1) echo '<tr>';
          
          
          $actorname =(count($actors)>1)?'('.$step['actor'].')':''; 
          ?>
                    
             <td>
-               Step 
-               <?php echo $step['number'];?><?php echo $actorname;?>
+                <b><?php echo $step['number'];?>.</b>               <?php echo $actorname;?>
                <?php echo Version::model()->wikiOutput($step['text'],1)." <br />".Version::model()->wikiOutput($step['result'],1)."</td> "; ?>
             </td>
 
