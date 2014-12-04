210229ed205356c3a700db729e4848dcc665b026
bfconsult <bfconsult@d823a327-aa83-4d46-b4ba-02dd668e2445>
2014-12-04 02:53:32 +0000

    fix edit permission check

--- req/protected/views/simple/_form.php
+++ req/protected/views/simple/_form.php
@@ -1,17 +1,9 @@
-<?php
-/* @var $this ObjectpropertyController */
-/* @var $model Objectproperty */
-/* @var $form CActiveForm */
-?>
+
 
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'categoryproperty-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 )); ?>
 
--- req/protected/views/simple/create.php
+++ req/protected/views/simple/create.php
@@ -1,11 +1,13 @@
 <?php 
 echo $this->renderPartial('/project/head',array('tab'=>'details'));
-
+$edit=(Yii::App()->session['edit']==1)?TRUE:FALSE;
 $permission=(Yii::App()->session['permission']==1)?true : false; 
 ?>
 <h3>Create A Simple Requirement</h3>
 
 <?php 
+
+
 if (isset($edit)){
 $this->renderPartial('_form', array('model'=>$model,'category_id'=>$category_id,)); 
 }
